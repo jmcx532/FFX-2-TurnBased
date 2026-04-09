205b39aee005a8b91c1a0a959ada140c8504f8ed
jmcx532 <jmcx532@hotmail.com>
2026-04-09 22:29:39 +0100

    Update to Fh v1.0.0-alpha09

--- src/usings.cs
+++ src/usings.cs
@@ -1,10 +1,4 @@
-﻿/* [fkelava 27/6/25 00:30]
- * For cleanliness, it is recommended that any namespaces you intend to use in more than one code file are imported
- * as 'global using' to permit you to use them anywhere without having to declare them in every file.
- *
- * 'global using' are project-scoped. They do not leak outside of your mod's source.
- */
-global using System;
+﻿global using System;
 global using System.IO;
 global using System.Linq;
 global using System.Numerics;
@@ -12,9 +6,4 @@ global using System.Data;
 global using System.Runtime.InteropServices;
 global using System.Collections.Generic;
 
-
 global using Hexa.NET.ImGui;
-
-global using Fahrenheit.Core;
-//global using Fahrenheit.Core.FFX;
-global using Fahrenheit.Core.FFX2;
